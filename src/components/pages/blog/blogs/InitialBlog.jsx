5e7e417d3521055dc50a3866b5386cf7e3dd7fdf
Abayvm <abayvmuraleedharan@gmail.com>
2025-05-14 13:42:25 +0530

    fix warning

--- src/components/pages/blog/blogs/InitialBlog.jsx
+++ src/components/pages/blog/blogs/InitialBlog.jsx
@@ -13,7 +13,7 @@ function InitialBlog(){
                 <p className="blog-date"><i>28-Aug-2024</i></p>
                 <p>
                     I have no idea what to write here or why I made this. Anyway I'm here now.<br /> I bought this domain <i>abayyy.xyz</i> couple weeks ago
-                    from goDaddy and made this website using React.js. Hosted it on <i>vercel</i>. You can check the code out here on <i><a href="https://github.com/Abayvm/portfolio" target="_blank">github</a></i>.
+                    from goDaddy and made this website using React.js. Hosted it on <i>vercel</i>. You can check the code out here on <i><a href="https://github.com/Abayvm/portfolio" target="_blank" rel="noreferrer">github</a></i>.
                     <br/>
                     I often look at other blogs, and I never even thought about how few people create their own, rather than read others blogs. Maybe I'll blog
                     about programming, books I read, movies I watch and about things I find interesting. <br />I hope I won't stop blogging because I'm a massive degenerate who pushes responsibilty than code..
